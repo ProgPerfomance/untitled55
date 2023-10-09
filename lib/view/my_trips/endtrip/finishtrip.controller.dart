29abf74c1af320caca24c7161835e5677a9e5773
Paul Iluhin <info@iluhin.pro>
2023-10-09 20:02:16 +0300

    ..

--- lib/view/my_trips/endtrip/finishtrip.controller.dart
+++ lib/view/my_trips/endtrip/finishtrip.controller.dart
@@ -1,8 +1,7 @@
 import 'package:get/get.dart';
 
-class FinishTripController extends GetxController {  void onEndTripTap() {
-  }
+class FinishTripController extends GetxController {
+  void onEndTripTap() {}
 
-  void onRequestTimeOffTap() {
-  }
+  void onRequestTimeOffTap() {}
 }
