a4783451cb92b25ede0dffc367fc62d27a5020ce
Paul Iluhin <info@iluhin.pro>
2023-10-09 21:03:50 +0300

    ...

--- lib/view/my_trips/history/history.controller.dart
+++ lib/view/my_trips/history/history.controller.dart
@@ -1,11 +1,14 @@
 import 'dart:developer';
 
 import 'package:get/get.dart';
+import 'package:untitled55/view/my_trips/history/historydetail.view.dart';
 
 class HistoryContoller extends GetxController {
   void onFilterTap() {
     log("onFilterTap");
   }
 
-  void showHistoryDetail() {}
+  void showHistoryDetail() {
+    Get.to(() => const HistoryDetailView());
+  }
 }
